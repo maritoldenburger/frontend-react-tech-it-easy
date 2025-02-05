631ef83c336fb628181c7f17ff82ccac5f9f404f
moldenburger <maritoldenburger@hotmail.com>
2025-02-05 16:31:45 +0100

    Finished up to 2d, working on 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,34 @@ import calculateSoldTVs from "./helpers/calculateSoldTVs.js";
 import calculateOriginalStock from "./helpers/calculateOriginalStock.js";
 import calculateTVsToSell from "./helpers/calculateTVsToSell.js";
 import getNameTV from "./helpers/getNameTV.js";
-import {bestSellingTv} from "./constants/inventory.js";
+import {bestSellingTv, inventory} from "./constants/inventory.js";
 import getPriceTV from "./helpers/getPriceTV.js";
 import getScreenSizes from "./helpers/getScreenSizes.js";
 import check from "./assets/check.png";
 import minus from "./assets/minus.png";
+import showOutcomeInConsole from "./constants/oefeningenopdracht1.js";
 
 function mostSoldFirst() {
-    console.log("Meest verkocht eerst");
+    inventory.sort((a, b) => {
+        return b.sold - a.sold;
+    });
+
+    console.log("Meest verkocht eerst", inventory);
 }
 
 function cheapestFirst() {
-    console.log("Goedkoopste eerst");
+    inventory.sort((a, b) => {
+        return a.price - b.price;
+    });
+
+    console.log("Goedkoopste eerst", inventory);
 }
 
 function bestForSportsFirst() {
+    inventory.sort((a, b) => {
+        return b.refreshRate - a.refreshRate;
+    })
+
     console.log("Meest geschikt voor sport eerst");
 }
 
@@ -42,27 +55,69 @@ function App() {
                             <h2>{calculateTVsToSell()}</h2>
                         </article>
                     </div>
+                    <h2>Alle merken</h2>
+                    <article>
+                        <ul>
+                            {inventory.map((tv) => {
+                                return <li key={tv.type}>{tv.name}</li>
+                            })}
+                        </ul>
+                    </article>
                     <h2>Best verkochte tv</h2>
-                    <article className="best-selling-tv-container">
+                    <article className="tv-container">
                         <span className="tv-image">
                             <img src={bestSellingTv.sourceImg} alt="Onze best verkochte tv"/>
                         </span>
-                        <div className="best-selling-tv-info">
+                        <div className="tv-info">
                             <h2>{getNameTV(bestSellingTv)}</h2>
                             <h2>{getPriceTV(bestSellingTv.price)}</h2>
                             <p>{getScreenSizes(bestSellingTv.availableSizes)}</p>
                             <ul className="tv-info-list">
-                                <li><img src={check} alt="Icoon: vinkje"/> wifi</li>
-                                <li><img src={minus} alt="Icoon: geen vinkje"/> speech</li>
-                                <li><img src={check} alt="Icoon: vinkje"/> hdr</li>
-                                <li><img src={check} alt="Icoon: vinkje"/> bluetooth</li>
-                                <li><img src={minus} alt="Icoon: geen vinkje"/> ambilight</li>
+                                <li><img src={check} alt="wel"/> wifi</li>
+                                <li><img src={minus} alt="niet"/> speech</li>
+                                <li><img src={check} alt="wel"/> hdr</li>
+                                <li><img src={check} alt="wel"/> bluetooth</li>
+                                <li><img src={minus} alt="niet"/> ambilight</li>
                             </ul>
                         </div>
                     </article>
-                    <button type="button" onClick={mostSoldFirst}>Meest verkocht eerst</button>
-                    <button type="button" onClick={cheapestFirst}>Goedkoopste eerst</button>
-                    <button type="button" onClick={bestForSportsFirst}>Meest geschikt voor sport eerst</button>
+                    <h2>Alle tvs</h2>
+                    <button type="button" onClick={mostSoldFirst}>
+                        Meest verkocht eerst
+                    </button>
+                    <button type="button" onClick={cheapestFirst}>
+                        Goedkoopste eerst
+                    </button>
+                    <button type="button" onClick={bestForSportsFirst}>
+                        Meest geschikt voor sport eerst
+                    </button>
+                    {inventory.map((tv) => {
+                        return <article className="tv-container" key={tv.type}>
+                            <span className="tv-image">
+                            <img src={tv.sourceImg} alt="Afbeelding van de tv"/>
+                            </span>
+                            <div className="tv-info">
+                                <h2>{getNameTV(tv)}</h2>
+                                <h2>{getPriceTV(tv.price)}</h2>
+                                <p>{getScreenSizes(tv.availableSizes)}</p>
+                                <ul className="options-list">
+                                    {tv.options.map((option) => {
+                                        if (option.applicable === true) {
+                                            return <li key={option.name}>
+                                                <img src={check} alt="wel"/>
+                                                {option.name}
+                                            </li>
+                                        } else {
+                                            return <li key={option.name}>
+                                                <img src={minus} alt="niet"/>
+                                                {option.name}
+                                            </li>
+                                        }
+                                    })}
+                                </ul>
+                            </div>
+                        </article>
+                    })}
                 </section>
             </main>
         </>
